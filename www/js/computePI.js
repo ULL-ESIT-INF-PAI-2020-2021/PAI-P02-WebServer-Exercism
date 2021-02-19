9f6fb216b811fba9c9a6f856003b4e89c5766ac1
fsande <fsande@ull.es>
2021-02-19 09:38:59 +0000

    Change the Pi computation using Riemmann Sums

--- www/js/computePI.js
+++ www/js/computePI.js
@@ -1,26 +1,31 @@
 /**
  * @author       F. de Sande
  * @since 27.01.2020
+ * @since 18.02.2021
+ * @see {@link https://towardsdatascience.com/how-to-make-pi-part-1-d0b41a03111f}
  * 
  * Compute PI 
+ * @description The code uses the method of Riemann Sums to approximate the area under a circle. 
+ * This approximation is equal to (pi / 4). For more on Riemann Sums, see here.
  * Uses prompt and alert for input/output
  */
 
-
-let steps = 0;
 function CalculatePI() {
-  let steps  = prompt("Enter number of steps:", "0");
-  if (steps > 0) {
+  let numRectangles = prompt("Enter number of rectangles:", "0");
+  if (numRectangles > 0) { 
+    let deltaX = 1.0 / numRectangles;
+    let x = 0;
     let Pi = 0;
-    let n = 1;
-    for (i = 0; i < steps; i++) {
-      Pi = Pi + (4 / n) - (4 / (n + 2));
-      n += 4;
-			debugger;
+    while (x < 1) {
+      let fx = Math.sqrt(1 - x * x);  // Circle with radius 1 and center at (0, 0)
+      Pi += fx * deltaX;  // rectangle area
+      x += deltaX;
+		  debugger;
     }
-  alert(Pi);
+    Pi = 4 * Pi;
+    alert(Pi);
   }
   else {
     alert("Canceled or Error in input: Input must be positive.");
-		}
+	}
 }
